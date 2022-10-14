82dd72e3d6c9ec9d345e4d87ae28b8db68bdaf18
Caleb O'Kane <caleb.ceo.okane@gmail.com>
2022-10-14 11:56:48 -0500

    testing things in the warm ups js

--- JS/warm-ups.js
+++ JS/warm-ups.js
@@ -664,6 +664,28 @@ $("#blinkPink").on('click', function (){
     }, 3000);
 })
 
+// let numbers = [1, 2, 3, 4, 5];
+// numbers.map(n => n * 3);
+// console.log(numbers.map(n => n * 3));
 
+// const isDivisibleBy3 = n => n % 3 === 0;
+//
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//
+// console.log(numbers.filter(isDivisibleBy3));
+// console.log(numbers);
+
+let x = 5;
+console.log(typeof x);
+
+function sayHello(name) {
+    return 'Hello, ' + name + '!';
+}
+
+const sayHello2 = (name) => { 'Hello, ' + name + '!' };
+
+const sayHello3 = name => { 'Hello, ' + name + '!' };
+
+const sayHello4 = (name) => `Hello, ${name}!`
 
 
